60b5c328f2cad43be1d1a4251e705e69cdfe1504
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-10-22 00:19:08 +0300

    mini registration done

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 require('dotenv').config();
 
-const { Bot, GrammyError, HttpError} = require('grammy');
+const { Bot, GrammyError, HttpError, InlineKeyboard} = require('grammy');
+const { hydrate } = require('@grammyjs/hydrate');
+const { session, MemorySessionStorage } = require('grammy');
 
 const bot = new Bot(process.env.BOT_API_KEY);
+bot.use(hydrate());
+
+// Настраиваем сессии
+bot.use(session({
+    initial: () => ({ step: null }),
+    storage: new MemorySessionStorage()
+}));
+
+const regKeyboard = new InlineKeyboard().text('Пройти регистрацию', 'reg');
 
 bot.command('start', async (ctx) => {
-    await ctx.reply('Привет!');
-})
+    // Проверка состояния регистрации
+    if (ctx.session.step === 'waiting_for_name') {
+        await ctx.reply('Вы ещё не завершили регистрацию. Пожалуйста, введите фамилию и имя.');
+        return;
+    }
+
+    // Стандартный ответ на команду /start
+    await ctx.reply('Привет! Это бот для записи на сдачу лабораторных работ.');
+    await ctx.reply('Давай пройдём регистрацию:', {
+        reply_markup: regKeyboard
+    });
+});
+
+
+bot.callbackQuery('reg', async (ctx) => {
+    await ctx.answerCallbackQuery();
+    await ctx.callbackQuery.message.editText('Чтобы пройти регистрацию, введите вашу фамилию и имя:\n\n_Например: Иванов Иван_', {
+        parse_mode: 'MarkdownV2'
+    });
+    ctx.session.step = 'waiting_for_name';
+});
 
 bot.on('message', async (ctx) => {
-    console.log(ctx.msg);
-})
+    if (ctx.session.step === 'waiting_for_name') {
+        const fullName = ctx.message.text;
+        
+        // Простая валидация ввода (можно расширить при необходимости)
+        if (!fullName.includes(' ')) {
+            await ctx.reply('Пожалуйста, введите корректные фамилию и имя в формате:\n\n_Иванов Иван_', {
+                parse_mode: 'MarkdownV2'
+            });
+            return;
+        }
+
+        // Сохраняем фамилию и имя (тут можно сохранить в базу данных или продолжить с другой логикой)
+        await ctx.reply(`Спасибо\\! Вы зарегистрированы как: _${fullName}_`, {
+            parse_mode: 'MarkdownV2'
+        } );
+        
+        // Очистка шага регистрации
+        ctx.session.step = null;
+    } else {
+        await ctx.reply('Я не понимаю это сообщение. Для начала нажмите /start.');
+    }
+});
 
 bot.catch((err) => {
     const ctx = err.ctx;
@@ -23,6 +73,6 @@ bot.catch((err) => {
     } else {
       console.error("Unknown error:", e);
     }
-  });
+});
 
 bot.start();
