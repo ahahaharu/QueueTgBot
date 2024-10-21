74772406aa9e660d96b8fd22175228c2f4760cf3
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-10-21 22:20:23 +0300

    error handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const {Bot} = require('grammy');
+const { Bot, GrammyError, HttpError} = require('grammy');
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -11,5 +11,18 @@ bot.command('start', async (ctx) => {
 bot.on('message', async (ctx) => {
     await ctx.reply ('...');
 })
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+  });
 
 bot.start();
