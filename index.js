0ad66d1c029fd997359b86b8f00a7ee143d37328
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-10-21 20:51:14 +0300

    bot created

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const {Bot} = require('grammy');
+
+const bot = new Bot(process.env.BOT_API_KEY);
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('Привет!');
+})
+
+bot.on('message', async (ctx) => {
+    await ctx.reply ('...');
+})
+
+bot.start();
