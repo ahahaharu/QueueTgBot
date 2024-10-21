643cc3a82aab1aaf968ef6702c904990e0f07044
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-10-22 01:17:35 +0300

    fix

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ const { session, MemorySessionStorage } = require('grammy');
 const bot = new Bot(process.env.BOT_API_KEY);
 bot.use(hydrate());
 
-// Настраиваем сессии
 bot.use(session({
     initial: () => ({ step: null }),
     storage: new MemorySessionStorage()
 }));
 
+// TODO: добавить меню
+
 const regKeyboard = new InlineKeyboard().text('Пройти регистрацию', 'reg');
+const yesOrNoKeyboard = new InlineKeyboard()
+    .text('Да', 'reg_yes')
+    .text('Нет', 'reg_no')
+
+// TODO: обработать кнопки
 
 bot.command('start', async (ctx) => {
+    console.log(ctx.msg);
     // Проверка состояния регистрации
     if (ctx.session.step === 'waiting_for_name') {
         await ctx.reply('Вы ещё не завершили регистрацию. Пожалуйста, введите фамилию и имя.');
         return;
     }
 
-    // Стандартный ответ на команду /start
     await ctx.reply('Привет! Это бот для записи на сдачу лабораторных работ.');
     await ctx.reply('Давай пройдём регистрацию:', {
         reply_markup: regKeyboard
@@ -51,12 +57,13 @@ bot.on('message', async (ctx) => {
         }
 
         // Сохраняем фамилию и имя (тут можно сохранить в базу данных или продолжить с другой логикой)
-        await ctx.reply(`Спасибо\\! Вы зарегистрированы как: _${fullName}_`, {
-            parse_mode: 'MarkdownV2'
+        await ctx.reply(`Спасибо\\! Вы зарегистрированы как: _${fullName}_\n\nВсё верно?`, {
+            parse_mode: 'MarkdownV2',
+            reply_markup: yesOrNoKeyboard
         } );
         
         // Очистка шага регистрации
-        ctx.session.step = null;
+        ctx.session.step = null; // TODO: после нажатия на "да", должна использоваться эта функция, а при нажатии на "нет" - не должна
     } else {
         await ctx.reply('Я не понимаю это сообщение. Для начала нажмите /start.');
     }
