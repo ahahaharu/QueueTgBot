b33dd25ad98a77860377be80ddce01918012c5d3
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-12-02 23:12:14 +0300

    db fix

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,26 +1,23 @@
 const mysql = require("mysql2");
 
-// Создаём пул соединений
 const pool = mysql.createPool({
-    port: "3306",
-    user: "gen_user",
-    host: "194.87.56.214",
-    database: "students",
-    password: "827530Ab.",
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 20,
     queueLimit: 0
 });
 
-// Проверка подключения к базе данных
 pool.getConnection((err, connection) => {
     if (err) {
         console.error("Ошибка подключения к базе данных:", err);
         return;
     }
     console.log("Подключено к базе данных через пул соединений!");
-    connection.release(); // Возвращаем соединение обратно в пул
+    connection.release(); 
 });
 
-// Экспортируем пул для использования в других модулях
 module.exports = pool;
