0486d0495f6b099cffb9f53ebca2799531ea4e1f
Andrey <112519966+ahahaharu@users.noreply.github.com>
2025-03-12 20:18:59 +0300

    fix

--- src/commands/statusCheck.js
+++ src/commands/statusCheck.js
@@ -2,14 +2,15 @@ const { lessons } = require("../../data/lessons");
 
 async function statusCheck(ctx) {
   const step = ctx.session.step;
+  console.log(step);
   if (step && step.startsWith("waiting_for_")) {
-    const lessonKey = step.replace("waiting_for_", "");
+    const lessonKey = step.replace("waiting_for_", "").replace("Lab", "");
 
     const lesson = lessons.find((ls) => ls.name === lessonKey);
 
-    if (lessonName) {
+    if (lessonKey) {
       await ctx.reply(
-        `Идёт запись на ${lesson.title}! \nПожалуйста, окончите запись, чтобы пользоваться другими командами.`,
+        `Идёт запись на ${lesson.title}\\! \nПожалуйста, окончите запись, чтобы пользоваться другими командами\\.`,
         { parse_mode: "MarkdownV2" }
       );
       return true;
